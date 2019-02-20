f786b635fe29cf280a3c11b6c94fcc83c98d597b
James Ault <james.ault@johnlewis.co.uk>
2019-02-20 16:59:23 +0000

    Passing tests for game class usage as global variable

--- app.rb
+++ app.rb
@@ -11,27 +11,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session["init"] = true
-    $player_1 = Player.new(params[:name1])
-    $player_2 = Player.new(params[:name2])
+    @player_1 = Player.new(params[:name1])
+    @player_2 = Player.new(params[:name2])
+    $game = Game.new(@player_1,@player_2)
     redirect to('/play')
   end
 
   get '/play' do
-    @name1 = $player_1.name
-    @name2 = $player_2.name
-    @hp1 = $player_1.hp
-    @hp2 = $player_2.hp
+    @name1 = $game.player_1.name
+    @name2 = $game.player_2.name
+    @hp1 = $game.player_1.hp
+    @hp2 = $game.player_2.hp
     erb :play
   end
 
   get '/attack' do
     @attacked_player = params[:name]
-    @player1 = $player_1
-    @player2 = $player_2
+    @player1 = $game.player_1
+    @player2 = $game.player_2
     if @attacked_player == @player1.name
-      Game.new.attack(@player1)
-    else Game.new.attack(@player2)
+      $game.attack(@player1)
+    else $game.attack(@player2)
     end
     erb :attack
   end
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,15 @@
 class Game
+  attr_reader :player_1, :player_2
+  def initialize(player1,player2)
+    @player_1 = player1
+    @player_2 = player2
+  end
 
   def attack(defender)
-    defender.attacked
+    if defender == @player1
+    @player_1.attacked
+    else 
+    @player_2.attacked
+    end
   end
 end
